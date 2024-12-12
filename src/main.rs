4bc5e0d2898b6684b78552439b37732e9dd95f41
yasuomaidana <yasuomaidana@gmail.com>
2024-12-11 21:21:20 -0600

    Organizing code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use winit::event_loop::{ControlFlow, EventLoop};
 use learning_wpu::App;
+use winit::event_loop::EventLoop;
+
+pub async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let mut app = App::default();
+    let _ = event_loop.unwrap().run_app(&mut app);
+}
 
 fn main() {
-    env_logger::init();
-
-    let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    let mut app = App::default();
-    if let Err(e) = event_loop.run_app(&mut app) {
-        eprintln!("Event loop error: {e}");
-    }
+    pollster::block_on(run());
 }
