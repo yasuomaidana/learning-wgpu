c2749495c742c7d505135eddf70a7a0429de1901
yasuomaidana <yasuomaidana@gmail.com>
2024-12-15 19:01:10 -0600

    Showing screen with hello world

--- the-surface/src/main.rs
+++ the-surface/src/main.rs
@@ -1,5 +1,14 @@
+use crate::state_app::StateApplication;
+
 mod state;
+mod state_app;
+
+pub async fn run(){
+    let event_loop = winit::event_loop::EventLoop::new();
+    let mut window_state = StateApplication::new();
+    let _ = event_loop.unwrap().run_app(&mut window_state);
+}
 
 fn main() {
-    println!("Hello, world!");
+    pollster::block_on(run());
 }
