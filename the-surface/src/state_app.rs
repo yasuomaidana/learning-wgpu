f1821965763d761f2bf3725b8817200dd62fe71b
yasuomaidana <yasuomaidana@gmail.com>
2024-12-15 19:00:23 -0600

    Defining State application

---
+++ the-surface/src/state_app.rs
@@ -0,0 +1,48 @@
+use crate::state::State;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::ActiveEventLoop;
+use winit::window::{Window, WindowId};
+
+pub struct StateApplication<'a> {
+    state: Option<State<'a>>,
+}
+
+impl<'a> StateApplication<'a> {
+    pub fn new() -> StateApplication<'a> {
+        StateApplication { state: None }
+    }
+}
+
+impl ApplicationHandler for StateApplication<'_> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop
+            .create_window(Window::default_attributes().with_title("Hello, World!"))
+            .expect("Failed to create window");
+        self.state = Some(State::new(window));
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        let window = self.state.as_ref().unwrap().window();
+
+        if window.id() == window_id {
+            match event {
+                WindowEvent::CloseRequested => {
+                    event_loop.exit();
+                }
+                WindowEvent::Resized(physical_size) => {
+                    self.state.as_mut().unwrap().resize(physical_size);
+                }
+                WindowEvent::RedrawRequested => {
+                    self.state.as_mut().unwrap().render().unwrap();
+                }
+                _ => {}
+            }
+        }
+    }
+}
