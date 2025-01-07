d76f95134274df7d0a1e933fc96d18c4f2669f44
yasuomaidana <yasuomaidana@gmail.com>
2025-01-07 09:30:25 -0600

    Formatting code using cargo fmt

--- common/src/event_handler/pressure_event_handler.rs
+++ common/src/event_handler/pressure_event_handler.rs
@@ -1,7 +1,4 @@
-pub fn handle_pressure_event(
-    pressure:f32,
-    previous_pressure: f64,
-) -> f64{
+pub fn handle_pressure_event(pressure: f32, previous_pressure: f64) -> f64 {
     if pressure == 0.0 && (previous_pressure - pressure as f64).abs() > 0.2 {
         previous_pressure - 0.1
     } else {
