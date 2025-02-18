db657d99e9f9e203478599d124da93318efee134
yasuomaidana <yasuomaidana@gmail.com>
2025-02-17 21:29:44 -0600

    COUR-1868: Defining triangle using vertex

---
+++ buffer-and-indices/src/lib/const_values.rs
@@ -0,0 +1,16 @@
+use crate::lib::vertex::Vertex;
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [0.0, 0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+];
