0d1c26780eb55dd52590249cc124ebd2bd570859
yasuomaidana <yasuomaidana@gmail.com>
2025-02-23 16:58:47 -0600

    COUR-1869: Updating pipeline generation without buffers

--- common/src/pipeline_builder.rs
+++ common/src/pipeline_builder.rs
@@ -70,42 +70,14 @@ pub fn create_render_pipeline(
     vertex_entrypoint: &str,
     fragment_entrypoint: &str,
 ) -> RenderPipeline {
-    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: Some(pipe_line_label),
-        layout: Some(pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: shader_module,
-            entry_point: Some(vertex_entrypoint),
-            buffers: &[],
-            // compilation_options: Default::default(),
-            compilation_options: PipelineCompilationOptions::default(),
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: shader_module,
-            entry_point: Some(fragment_entrypoint),
-            targets: &[Some(wgpu::ColorTargetState {
-                format: config.format,
-                blend: Some(wgpu::BlendState::REPLACE),
-                write_mask: wgpu::ColorWrites::ALL,
-            })],
-            compilation_options: PipelineCompilationOptions::default(),
-        }),
-        primitive: wgpu::PrimitiveState {
-            topology: wgpu::PrimitiveTopology::TriangleList,
-            strip_index_format: None,
-            front_face: wgpu::FrontFace::Ccw,
-            cull_mode: Some(wgpu::Face::Back),
-            unclipped_depth: false,
-            polygon_mode: wgpu::PolygonMode::Fill,
-            conservative: false,
-        },
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState {
-            count: 1,
-            mask: !0,
-            alpha_to_coverage_enabled: false,
-        },
-        multiview: None,
-        cache: None,
-    })
+    create_render_pipeline_with_buffers(
+        device,
+        pipeline_layout,
+        shader_module,
+        config,
+        pipe_line_label,
+        vertex_entrypoint,
+        fragment_entrypoint,
+        &[],
+    )
 }
