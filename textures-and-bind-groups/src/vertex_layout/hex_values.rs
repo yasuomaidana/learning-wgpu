43378c07108b97466e5f326711ae5a30eab1bc9a
yasuomaidana <yasuomaidana@gmail.com>
2025-04-22 23:25:28 -0600

    COUR-1889: Modifying vertex to use textures instead of colors.

--- textures-and-bind-groups/src/vertex_layout/hex_values.rs
+++ textures-and-bind-groups/src/vertex_layout/hex_values.rs
@@ -3,27 +3,27 @@ use crate::vertex_layout::vertex::Vertex;
 pub const HEX_VERTICES: &[Vertex] = &[
     Vertex {
         position: [0.1961161, -0.1961161, 0.0],
-        color: [0.00, 0.50, 0.00],
+        tex_coords: [0.4131759, 0.99240386],
     },
     Vertex {
         position: [-0.0980581, 0.3134085, 0.0],
-        color: [0.00, 0.50, 0.00],
+        tex_coords: [0.0048659444, 0.56958647],
     },
     Vertex {
         position: [-0.6864065, 0.3134085, 0.0],
-        color: [0.00, 0.50, 0.00],
+        tex_coords: [0.28081453, 0.05060294],
     },
     Vertex {
         position: [-0.9805807, -0.1961161, 0.0],
-        color: [0.00, 0.50, 0.00],
+        tex_coords: [0.85967, 0.1526709],
     },
     Vertex {
         position: [-0.6864065, -0.7056408, 0.0],
-        color: [0.00, 0.50, 0.00],
+        tex_coords: [0.9414737, 0.7347359],
     },
     Vertex {
         position: [-0.0980581, -0.7056408, 0.0],
-        color: [0.00, 0.50, 0.00],
+        tex_coords: [0.9414737, 0.99240386],
     },
 ];
 
--- textures-and-bind-groups/src/vertex_layout/vertex.rs
+++ textures-and-bind-groups/src/vertex_layout/vertex.rs
@@ -23,38 +23,15 @@ use wgpu::vertex_attr_array;
 #[derive(Copy, Clone, Debug, Pod, Zeroable)]
 pub struct Vertex {
     pub(crate) position: [f32; 3],
-    pub(crate) color: [f32; 3],
+    // pub(crate) color: [f32; 3], now we use textures
+    pub(crate) tex_coords: [f32; 2],
 }
 
 impl<'a> Vertex {
     /// Vertex attributes for the `Vertex` struct.
     /// This array defines the layout of the vertex data in the buffer.
     const ATTRIBUTES: [wgpu::VertexAttribute; 2] =
-        vertex_attr_array![0 => Float32x3, 1 => Float32x3];
-
-    /// Long way
-    ///
-    /// ```rust
-    /// fn desc() -> wgpu::VertexBufferLayout<'static> {
-    ///         wgpu::VertexBufferLayout {
-    ///             array_stride: size_of::<Vertex>() as wgpu::BufferAddress,
-    ///             step_mode: wgpu::VertexStepMode::Vertex,
-    ///             attributes: &[
-    ///                 wgpu::VertexAttribute {
-    ///                     offset: 0,
-    ///                     shader_location: 0,
-    ///                     format: wgpu::VertexFormat::Float32x3,
-    ///                 },
-    ///                 wgpu::VertexAttribute {
-    ///                     offset: size_of::<[f32; 3]>() as wgpu::BufferAddress,
-    ///                     shader_location: 1,
-    ///                     format: wgpu::VertexFormat::Float32x3,
-    ///                 },
-    ///             ],
-    ///         }
-    ///     }
-    /// ```
-    ///
+        vertex_attr_array![0 => Float32x3, 1 => Float32x2];
 
     pub fn desc() -> wgpu::VertexBufferLayout<'a> {
         wgpu::VertexBufferLayout {
