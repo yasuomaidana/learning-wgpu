15ea8e7088f41e5fe6e15f97a4ba932887f7d8dc
yasuomaidana <yasuomaidana@gmail.com>
2025-04-23 20:01:15 -0600

    COUR-1900: Exposing texture_builder.rs

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,6 +3,7 @@ use winit::application::ApplicationHandler;
 pub mod event_handler;
 pub mod pipeline_builder;
 pub mod state_builder;
+pub mod texture_builder;
 
 pub async fn run<T: ApplicationHandler + 'static>(app: T) {
     let event_loop = winit::event_loop::EventLoop::new();
