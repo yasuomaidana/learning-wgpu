566a7c918a68a13448b62a86d99aba3d96a46c19
yasuomaidana <yasuomaidana@gmail.com>
2025-05-12 18:52:18 -0600

    COUR-1909: Defining camera

---
+++ uniform-buffers-an-camera/src/camera.rs
@@ -0,0 +1,30 @@
+pub struct Camera {
+    eye: cgmath::Point3<f32>, // camera position
+    target: cgmath::Point3<f32>, // look at this position
+    up: cgmath::Vector3<f32>, // up direction
+    aspect: f32, // aspect ratio of the window width/height
+    fovy: f32, // field of view in the y direction
+    znear: f32,
+    zfar: f32,
+}
+
+impl Camera {
+    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        // 1. The view matrix moves the world to be at the position and rotation of the camera. 
+        // It's essentially an inverse of whatever the transform matrix of the camera would be.
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        // 2. The proj matrix warps the scene to give the effect of depth. 
+        // Without this, objects up close would be the same size as objects far away.
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+        // 3. The coordinate system in WGPU is based on DirectX and Metal's coordinate systems.
+        OPENGL_TO_WGPU_MATRIX * proj * view
+    }
+}
+
+#[rustfmt::skip]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.5,
+    0.0, 0.0, 0.0, 1.0,
+);
