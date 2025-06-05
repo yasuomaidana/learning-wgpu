a9c06cac94dfe0d9e5d0524c587b1242121933b2
yasuomaidana <yasuomaidana@gmail.com>
2025-06-04 22:44:24 -0600

    COUR-1909: Creating camera uniform

--- uniform-buffers-an-camera/src/camera.rs
+++ uniform-buffers-an-camera/src/camera.rs
@@ -1,9 +1,9 @@
 pub struct Camera {
-    pub(crate) eye: cgmath::Point3<f32>, // camera position
+    pub(crate) eye: cgmath::Point3<f32>,    // camera position
     pub(crate) target: cgmath::Point3<f32>, // look at this position
-    pub(crate) up: cgmath::Vector3<f32>, // up direction
-    pub(crate) aspect: f32, // aspect ratio of the window width/height
-    pub(crate) fovy: f32, // field of view in the y direction
+    pub(crate) up: cgmath::Vector3<f32>,    // up direction
+    pub(crate) aspect: f32,                 // aspect ratio of the window width/height
+    pub(crate) fovy: f32,                   // field of view in the y direction
     pub(crate) znear: f32,
     pub(crate) zfar: f32,
 }
@@ -31,3 +31,25 @@ pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
     0.0, 0.0, 0.5, 0.5,
     0.0, 0.0, 0.0, 1.0,
 );
+
+// We need this for Rust to store our data correctly for the shaders
+#[repr(C)]
+// This is so we can store this in a buffer
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct CameraUniform {
+    pub view_proj: [[f32; 4]; 4],
+}
+
+impl CameraUniform {
+    pub fn new() -> Self {
+        use cgmath::Matrix4;
+        use cgmath::SquareMatrix;
+
+        let view_proj = Matrix4::identity().into();
+        CameraUniform { view_proj }
+    }
+
+    pub fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.build_view_projection_matrix().into();
+    }
+}
