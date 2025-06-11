5dc0bb31626fd7b4671a53bfb225348888912d72
yasuomaidana <yasuomaidana@gmail.com>
2025-06-11 12:14:31 -0600

    COUR-1909: Creating DefaultKeyboardHandler trait and KeyHandler super trait

--- common/src/key_q_s_handler/keyboard_handler.rs
+++ common/src/key_q_s_handler/keyboard_handler.rs
@@ -1,44 +1,36 @@
-use crate::event_handler::event::InputEventTrait;
-use crate::event_handler::keyboard_handler::keyboard_input::SingleKeyboardInput;
+use winit::event::WindowEvent;
 
-#[derive(Debug)]
-pub enum Action {
-    Quit,
-    ChangeFigure,
+/// Trait providing default methods for keyboard handlers.
+///
+/// Types implementing this trait must define an associated `Action` type,
+/// and provide methods to handle input and clear state.
+pub trait DefaultKeyboardHandlerMethods {
+    type Action;
+    // self refers to the instance of the type (like this in other languages).
+    // In method signatures, &mut self means the method takes a mutable
+    // reference to the instance.
+    //
+    //
+    // Self (with a capital S) refers to the implementing type of the trait.
+    // In type `Action;`, `Self::Action` means the associated type Action defined
+    // for the type that implements the trait.
+    fn input(&mut self, event: &WindowEvent);
+    fn clear(&mut self);
 }
 
-pub struct KeyboardHandler {
-    actions: Vec<SingleKeyboardInput>,
-}
-
-impl KeyboardHandler {
-    pub fn new() -> KeyboardHandler {
-        KeyboardHandler {
-            actions: vec![
-                SingleKeyboardInput::new("Q".to_string()),
-                SingleKeyboardInput::new("S".to_string()),
-            ],
-        }
-    }
-
-    pub fn input(&mut self, event: &winit::event::WindowEvent) {
-        self.actions
-            .iter_mut()
-            .for_each(|action| action.update(event));
-    }
-
-    pub fn get_action(&mut self) -> Option<Action> {
-        match self.actions.iter().find(|action| action.in_progress()) {
-            None => None,
-            Some(action) => match action.key.as_str() {
-                "q" => Some(Action::Quit),
-                "s" => Some(Action::ChangeFigure),
-                _ => None,
-            },
-        }
-    }
-
-    pub fn clear(&mut self) {
-        self.actions.iter_mut().for_each(|action| action.clear());
-    }
+/// Trait for handling keyboard input and mapping it to actions.
+/// Types implementing this trait must define an associated `Action` type,
+/// provide a constructor, and a method to retrieve the next action.
+pub trait KeysHandler: DefaultKeyboardHandlerMethods {
+    type Action;
+    fn new() -> Self;
+    // Here, `<Self as KeysHandler>::Action` explicitly refers to the Action
+    // associated type from the KeysHandler trait, not from any supertrait
+    // (like DefaultKeyboardHandlerMethods).
+    //
+    // This is necessary because both traits define an Action associated type,
+    // and Rust needs to know which one you mean.
+    // Using <Self as KeysHandler>::Action removes ambiguity and ensures you get
+    // the correct associated type from the intended trait
+    fn get_action(&mut self) -> Option<<Self as KeysHandler>::Action>;
 }
