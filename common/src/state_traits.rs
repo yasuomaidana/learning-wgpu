d8130e7bf1fab292fdaee48f7eb639d51a48128c
yasuomaidana <yasuomaidana@gmail.com>
2025-06-11 17:21:09 -0600

    COUR-1909: Creating default_event and implement_resumed macros, it completes repeated code

--- common/src/state_traits.rs
+++ common/src/state_traits.rs
@@ -14,3 +14,32 @@ pub trait AppState: DefaultAppStateMethods {
     fn new(window: Window) -> Self;
     fn render(&mut self) -> Result<(), wgpu::SurfaceError>;
 }
+
+#[macro_export]
+macro_rules! implement_resumed {
+    () => {
+        fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+            let window = event_loop
+                .create_window(Window::default_attributes().with_title(self.window_name.as_str()))
+                .expect("Failed to create window");
+            self.state = Some(AppState::new(window));
+        }
+    };
+}
+#[macro_export]
+macro_rules! default_event {
+    ($self:ident, $event_loop:ident, $event:ident) => {
+        match $event {
+            WindowEvent::CloseRequested => {
+                $event_loop.exit();
+            }
+            WindowEvent::Resized(physical_size) => {
+                $self.state.as_mut().unwrap().resize(physical_size);
+            }
+            WindowEvent::RedrawRequested => {
+                $self.state.as_mut().unwrap().render().unwrap();
+            }
+            _ => {}
+        }
+    };
+}
